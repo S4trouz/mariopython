48ef5892dae931f0c4b049ed1b1f6b87c66d415c
Louis Gallego <louisgpro201@gmail.com>
2024-05-07 18:04:02 +0200

    update

--- Mario.py
+++ Mario.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 pygame.init()
 
 #Générer la fenêtre du jeu 
@@ -17,6 +18,8 @@ marioImg=pygame.image.load('mario.png')
 marioImg=pygame.transform.scale(marioImg,(70,70))
 marioX = 270
 marioY = 380
+marioX_change=0
+marioY_change=0
 
 def mario():
     screen.blit(marioImg, (marioX,marioY))
@@ -24,8 +27,8 @@ def mario():
 #Goomba
 goombaImg=pygame.image.load('goomba.png')
 goombaImg=pygame.transform.scale(goombaImg,(70,70))
-goombaX = 400
-goombaY = 450
+goombaX = random.randint(0, 730)
+goombaY = random.randint(0, 530)
 
 def goomba():
     screen.blit(goombaImg, (goombaX,goombaY))
@@ -33,8 +36,8 @@ def goomba():
 #piece
 coinImg=pygame.image.load('coin.png')
 coinImg=pygame.transform.scale(coinImg,(70,70))
-coinX = 100
-coinY= 200
+coinX = random.randint(0,730)
+coinY= random.randint(0,530)
 
 def coin():
     screen.blit(coinImg, (coinX,coinY))
@@ -43,6 +46,8 @@ def coin():
 running=True
 
 while running:
+    screen.fill(color)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running=False  
@@ -55,12 +60,19 @@ while running:
             if event.key == pygame.K_RIGHT:
                 marioX_change = 1
             if event.key == pygame.K_UP:
-                marioY_change = 1
-            if event.key == pygame.K_DOWN:
                 marioY_change = -1
+            if event.key == pygame.K_DOWN:
+                marioY_change = 1
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                marioX_change = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                marioY_change = 0
+
 
     marioX += marioX_change
     marioY += marioY_change
+
     if marioX <=0:
         marioX = 0
     elif marioX >=750:
@@ -69,6 +81,15 @@ while running:
         marioY = 0
     elif marioY >=550:
         marioY = 550
+    
+    #Collision avec la pièce
+    if marioX < coinX + 70 and marioX + 70 > coinX and marioY < coinY + 70 and marioY + 70 > coinY:
+        coinX = random.randint(0,730)
+        coinY = random.randint(0,530)
+
+        goombaX = random.randint(0,730)
+        goombaY = random.randint(0,530)
+    
     coin()
     mario()
     goomba()
